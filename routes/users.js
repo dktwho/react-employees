7c68c329e3081749cd9ac0a858303a8c303bbe65
Alexander Korobov <dktwho@mail.ru>
2024-01-23 17:16:26 +0300

    feat: create controllers with function login, register, current.

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
+
 // api/user/login
-router.post('/login', function(req, res, next) {
+router.post('/login', function(req, res ) {
   res.send('login');
 });
 
 // api/user/register
-router.post('/register', function(req, res, next) {
+router.post('/register', function(req, res ) {
   res.send('register');
 });
 
 // api/user/current
-router.post('/current', function(req, res, next) {
+router.get('/current', function(req, res) {
   res.send('current');
 });
 
