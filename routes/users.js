d6b82f0337be7443f671e7edf0fd76771f91051a
Alexander Korobov <dktwho@mail.ru>
2024-01-23 17:18:23 +0300

    feat: import function to routes users

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const {login, register, current} =require('../controllers/users')
 
 /* GET users listing. */
 
 // api/user/login
-router.post('/login', function(req, res ) {
-  res.send('login');
-});
+router.post('/login',login);
 
 // api/user/register
-router.post('/register', function(req, res ) {
-  res.send('register');
-});
+router.post('/register', register);
 
 // api/user/current
-router.get('/current', function(req, res) {
-  res.send('current');
-});
-
+router.get('/current', current);
 
 module.exports = router;
