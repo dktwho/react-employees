f731678112e5c64f33a44ce007084ac981ee1ba9
Alexander Korobov <dktwho@mail.ru>
2024-01-23 18:51:50 +0300

    feat:add auth middleware before current routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {login, register, current} =require('../controllers/users')
+const {login, register, current} = require('../controllers/users')
+const { auth } = require('../middlewares/auth')
 
 /* GET users listing. */
 
@@ -11,6 +12,6 @@ router.post('/login', login);
 router.post('/register', register);
 
 // api/user/current
-router.get('/current', current);
+router.get('/current', auth, current);
 
 module.exports = router;
