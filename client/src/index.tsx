adfb127c3197cc60367bc27edf217da501e6c7ea
Alexander Korobov <dktwho@mail.ru>
2024-01-25 14:34:34 +0300

    feat: create route, login. register, home

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,8 +2,28 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
 import reportWebVitals from './reportWebVitals';
+
 import './index.css';
+import {Paths} from "./paths";
+
+
+const router = createBrowserRouter([
+    {
+        path: Paths.home,
+        element: <h1>Employees</h1>
+    },
+    {
+        path: Paths.login,
+        element: <h1>login</h1>
+    },
+    {
+        path: Paths.register,
+        element: <h1>register</h1>
+    }
+])
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -11,7 +31,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-        <h1>Hello world</h1>
+        <RouterProvider router={router} />
+
+
     </Provider>
   </React.StrictMode>
 );
