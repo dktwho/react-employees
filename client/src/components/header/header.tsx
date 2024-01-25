fbd09618cb4d74ea8fb97329d19bbde9aeaa3bb7
Alexander Korobov <dktwho@mail.ru>
2024-01-25 15:29:30 +0300

    feat: add custom button to header, wrap with Link from rrd with paths.home - test

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -1,13 +1,21 @@
 import s from './header.module.css'
-import {Button, Layout, Space, Typography} from "antd";
+import { Layout, Space, Typography} from "antd";
 import {TeamOutlined} from "@ant-design/icons";
+import {CustomButton} from "../custom-button/custom-button";
+import {Link} from "react-router-dom";
+import {Paths} from "../../paths";
 
 export const Header = () => {
     return (
         <Layout.Header className={s.header}>
             <Space>
                 <TeamOutlined className={s.teamIcon}/>
-                <Button type={'link'}>Click</Button>
+                <Link to={Paths.home}>
+                    <CustomButton  ghost={true}>
+                        <Typography.Title level={4}>Сотрудники</Typography.Title>
+                    </CustomButton>
+                </Link>
+
             </Space>
         </Layout.Header>
     );
