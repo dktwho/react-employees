b3fea6de94cf7c24c1a16e8da19a308f5101afc0
Alexander Korobov <dktwho@mail.ru>
2024-01-25 16:04:35 +0300

    feat:create custom input password-input

---
+++ client/src/components/password-input/password-input.tsx
@@ -0,0 +1,15 @@
+import {Form, Input} from "antd";
+import {NamePath} from 'antd/es/form/interface'
+
+type Props = {
+    name: string;
+    placeholder: string;
+    dependencies?: NamePath[]
+}
+export const PasswordInput = ({name, placeholder, dependencies}: Props) => {
+    return (
+        <Form.Item name={name} dependencies={dependencies} hasFeedback rules={[]}>
+            <Input.Password placeholder={placeholder} size={'large'}/>
+        </Form.Item>
+    );
+};
