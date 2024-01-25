36c561aa0b4d400ec038d96b27e56271649faf19
Alexander Korobov <dktwho@mail.ru>
2024-01-25 17:33:20 +0300

    feat: finish onLogin func in login pages, add useState for error

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Layout} from "../../components/layout/layout";
 import {Card, Form, Row, Space, Typography} from "antd";
 import {CustomInput} from "../../components/custom-input/custom-input";
@@ -7,15 +7,22 @@ import {CustomButton} from "../../components/custom-button/custom-button";
 import {Link} from "react-router-dom";
 import {Paths} from "../../paths";
 import {useLoginMutation, UserData} from "../../app/services/auth";
+import {isErrorWithMessage} from "../../utils/isErrorWithMessage";
 
 export const Login = () => {
     const [loginUser, loginUserResult] = useLoginMutation()
+    const [error, setError] = useState('')
 
     const onLogin = async (data: UserData) => {
         try {
             await loginUser(data).unwrap();
         } catch (err) {
-
+            const maybeError = isErrorWithMessage(err)
+            if (maybeError) {
+                setError(err.data.message)
+            } else {
+                setError('Неизвестная ошибка')
+            }
         }
     }
     return (
