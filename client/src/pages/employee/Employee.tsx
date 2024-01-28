6f16672f03477d15bb36e1cc47743c9cab7d3d8d
Alexander Korobov <dktwho@mail.ru>
2024-01-28 16:47:38 +0300

    feat:add view info about user,Descriptions andt and D.Item

--- client/src/pages/employee/Employee.tsx
+++ client/src/pages/employee/Employee.tsx
@@ -1,8 +1,44 @@
+import {Navigate, useNavigate, useParams} from "react-router-dom";
+import {useState} from "react";
+import {useGetEmployeeQuery, useRemoveEmployeeMutation} from "../../app/services/employees";
+import {useSelector} from "react-redux";
+import {selectUser} from "../../features/auth/authSlice";
+import {Layout} from "../../components/layout/layout";
+import {Descriptions} from "antd";
 
 export const Employee = () => {
+    const navigate = useNavigate()
+    const [error, setError] = useState('')
+    const params = useParams<{ id: string }>()
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const {data, isLoading} = useGetEmployeeQuery(params.id || "")
+    const [removeEmployee] = useRemoveEmployeeMutation()
+    const user = useSelector(selectUser)
+
+    if (isLoading) {
+        return <span>Загрузка...</span>
+    }
+
+    if (!data) {
+        return <Navigate to={'/'}/>
+    }
+
     return (
-        <div>
-            Employee every page
-        </div>
+        <Layout>
+            <Descriptions title={'Информация о сотруднике'} bordered>
+                <Descriptions.Item label={'Имя'} span={3}>
+                    {`${data.firstName} ${data.lastName}`}
+                </Descriptions.Item>
+
+                <Descriptions.Item label={'Возраст'} span={3}>
+                    {`${data.age}`}
+                </Descriptions.Item>
+
+                <Descriptions.Item label={'Адрес'} span={3}>
+                    {`${data.address}`}
+                </Descriptions.Item>
+
+            </Descriptions>
+        </Layout>
     );
 };
