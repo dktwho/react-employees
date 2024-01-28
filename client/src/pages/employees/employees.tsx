a1d40be5e0614c083700c63d95336351d0e8f3a6
Alexander Korobov <dktwho@mail.ru>
2024-01-28 15:26:50 +0300

    feat:modified component Employees, add tables from antd, create columns type, view employees list

--- client/src/pages/employees/employees.tsx
+++ client/src/pages/employees/employees.tsx
@@ -1,8 +1,29 @@
+import {Layout} from "../../components/layout/layout";
+import {CustomButton} from "../../components/custom-button/custom-button";
+import {PlusCircleOutlined} from "@ant-design/icons";
+import {Table} from "antd";
+import {useGetAllEmployeesQuery} from "../../app/services/employees";
+import type {ColumnsType} from "antd/es/table";
+import {Employee} from "@prisma/client";
+
+const columns: ColumnsType<Employee> = [
+    {title: 'Имя', dataIndex: 'firstName', key: 'firstName'},
+    {title: 'Возраст', dataIndex: 'age', key: 'age'},
+    {title: 'Адрес', dataIndex: 'address', key: 'address'},
+]
+
 
 export const Employees = () => {
+    const {data, isLoading} = useGetAllEmployeesQuery()
     return (
-        <div>
-            employees page
-        </div>
+        <Layout>
+            <CustomButton type={'primary'} onClick={() => null} icon={<PlusCircleOutlined/>}>Добавить</CustomButton>
+            <Table loading={isLoading}
+                   dataSource={data}
+                   pagination={false}
+                   columns={columns}
+                   rowKey={(record) => record.id}/>
+        </Layout>
+
     );
 };
