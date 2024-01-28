71aca27242897ee3a7daf7afbdb3806266311991
Alexander Korobov <dktwho@mail.ru>
2024-01-28 15:32:38 +0300

    feat:add onRow antd, navigate to changed employee (id)

--- client/src/pages/employees/employees.tsx
+++ client/src/pages/employees/employees.tsx
@@ -5,6 +5,9 @@ import {Table} from "antd";
 import {useGetAllEmployeesQuery} from "../../app/services/employees";
 import type {ColumnsType} from "antd/es/table";
 import {Employee} from "@prisma/client";
+import {useNavigate} from "react-router-dom";
+import {Paths} from '../../paths'
+
 
 const columns: ColumnsType<Employee> = [
     {title: 'Имя', dataIndex: 'firstName', key: 'firstName'},
@@ -14,6 +17,7 @@ const columns: ColumnsType<Employee> = [
 
 
 export const Employees = () => {
+    const navigate = useNavigate()
     const {data, isLoading} = useGetAllEmployeesQuery()
     return (
         <Layout>
@@ -22,7 +26,12 @@ export const Employees = () => {
                    dataSource={data}
                    pagination={false}
                    columns={columns}
-                   rowKey={(record) => record.id}/>
+                   rowKey={(record) => record.id}
+                   onRow={(record) => {
+                       return {
+                           onClick: () => navigate(`${Paths.employee}/${record.id}`)
+                       }
+                   }}/>
         </Layout>
 
     );
