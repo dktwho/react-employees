9d9f011265d10aed6fd0d3bf01aa2b46ea710af2
Alexander Korobov <dktwho@mail.ru>
2024-01-28 16:57:51 +0300

    feat:Employee component modified, add conditional render, create modal window from antd

--- client/src/pages/employee/Employee.tsx
+++ client/src/pages/employee/Employee.tsx
@@ -1,10 +1,13 @@
-import {Navigate, useNavigate, useParams} from "react-router-dom";
+import {Link, Navigate, useNavigate, useParams} from "react-router-dom";
 import {useState} from "react";
 import {useGetEmployeeQuery, useRemoveEmployeeMutation} from "../../app/services/employees";
 import {useSelector} from "react-redux";
 import {selectUser} from "../../features/auth/authSlice";
 import {Layout} from "../../components/layout/layout";
-import {Descriptions} from "antd";
+import {Descriptions, Divider, Modal, Space} from "antd";
+import {CustomButton} from "../../components/custom-button/custom-button";
+import {DeleteOutlined, EditOutlined} from "@ant-design/icons";
+import {ErrorMessage} from "../../components/error-message/error-message";
 
 export const Employee = () => {
     const navigate = useNavigate()
@@ -39,6 +42,28 @@ export const Employee = () => {
                 </Descriptions.Item>
 
             </Descriptions>
+            {user?.id === data.userId && (
+                <>
+                    <Divider orientation={'left'}>
+                        Действие
+                    </Divider>
+                    <Space>
+                        <Link to={`/employee/edit/${data.id}`}>
+                            <CustomButton shape={'round'} type={'default'} icon={<EditOutlined/>}>
+                                Редактировать
+                            </CustomButton>
+                        </Link>
+                        <CustomButton shape={'round'} danger onClick={() => null}
+                                      icon={<DeleteOutlined/>}>Удалить</CustomButton>
+                    </Space>
+                </>
+            )}
+
+            <ErrorMessage message={error}/>
+            <Modal title={'Подтвердите удаление'} open={isModalOpen} onOk={() => null} onCancel={() => null}
+                   okText={'Подтвердить'} cancelText={'Отменить'}>Вы действительно хотите удалить сотрудника из таблицы ?
+
+            </Modal>
         </Layout>
     );
 };
