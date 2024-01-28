4a7f87687d129cacc42229cca9946239de31c9d5
Alexander Korobov <dktwho@mail.ru>
2024-01-28 14:36:52 +0300

    feat: check token and headers set

--- client/src/app/services/api.ts
+++ client/src/app/services/api.ts
@@ -5,6 +5,10 @@ const baseQuery = fetchBaseQuery({
     baseUrl: 'http://localhost:8000/api',
     prepareHeaders(headers, {getState}) {
         const token = (getState() as RootState).auth.user?.token || localStorage.getItem('token')
+
+        if (token && token !== null) {
+            headers.set('authorization', `Bearer  ${token}`)
+        }
     }
 });
 
