0947ba602c0135a836bd7f4aa61eebd03ab168b0
Alexander Korobov <dktwho@mail.ru>
2024-01-28 16:05:20 +0300

    feat:modified AddEmployee, add navigate try/catch for error, useState for error, create handleAddEmployee

--- client/src/pages/addEmployee/AddEmployee.tsx
+++ client/src/pages/addEmployee/AddEmployee.tsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Layout} from "../../components/layout/layout";
 import {Row} from "antd";
+import {EmployeeForm} from "../../components/employee-form/EmployeeForm";
+import {useSelector} from "react-redux";
+import {selectUser} from "../../features/auth/authSlice";
+import {useAddEmployeeMutation} from "../../app/services/employees";
+import {useNavigate} from "react-router-dom";
+import {Employee} from "@prisma/client";
+import {Paths} from "../../paths";
+import {isErrorWithMessage} from "../../utils/isErrorWithMessage";
 
 export const AddEmployee = () => {
-    return (
-       <Layout>
-           <Row align={'middle'} justify={'center'}>
+    const [error, setError] = useState('')
+    const navigate = useNavigate()
+    const user = useSelector(selectUser)
+    const [addEmployee] = useAddEmployeeMutation()
 
-           </Row>
-       </Layout>
+    useEffect(() => {
+        if (!user) {
+            navigate('/login')
+        }
+    }, [navigate, user])
+    const handleAddEmployee = async (data: Employee) => {
+        try {
+            await addEmployee(data).unwrap();
+            navigate(`${Paths.status}/created`)
+        } catch (err)  {
+            const mayBeError = isErrorWithMessage(err)
+            if(mayBeError) {
+                setError(err.data.message)
+            } else {
+                setError('Неизвестная ошибка')
+            }
+        }
+    }
+    return (
+        <Layout>
+            <Row align={'middle'} justify={'center'}>
+                <EmployeeForm onFinish={handleAddEmployee} btnText={'Добавить'} title={'добавить сотрудника'}
+                              error={error}/>
+            </Row>
+        </Layout>
     );
 };
