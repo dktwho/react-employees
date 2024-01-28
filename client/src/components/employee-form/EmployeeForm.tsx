5bd0703ae5362eccd50a2df95a57c042b63d0b8d
Alexander Korobov <dktwho@mail.ru>
2024-01-28 15:59:09 +0300

    feat:finish EmployeeForm, add custom input, error message and button

--- client/src/components/employee-form/EmployeeForm.tsx
+++ client/src/components/employee-form/EmployeeForm.tsx
@@ -1,4 +1,8 @@
 import {Employee} from "@prisma/client";
+import {Card, Form, Space} from "antd";
+import {CustomInput} from "../custom-input/custom-input";
+import {ErrorMessage} from "../error-message/error-message";
+import {CustomButton} from "../custom-button/custom-button";
 
 type Props<T> = {
     onFinish: (values: T) => void,
@@ -9,8 +13,17 @@ type Props<T> = {
 }
 export const EmployeeForm = ({onFinish, btnText, title, error, employee}: Props<Employee>) => {
     return (
-        <div>
-
-        </div>
+        <Card title={title} style={{width: '30rem'}}>
+            <Form name={'employee-form'} onFinish={onFinish} initialValues={employee}>
+                <CustomInput type={'text'} name={'firstName'} placeholder={'имя'}/>
+                <CustomInput type={'text'} name={'lastName'} placeholder={'фамилия'}/>
+                <CustomInput type={'number'} name={'age'} placeholder={'возраст'}/>
+                <CustomInput type={'text'} name={'address'} placeholder={'адрес'}/>
+                <Space>
+                    <ErrorMessage message={error}/>
+                    <CustomButton htmlType={'submit'}>{btnText}</CustomButton>
+                </Space>
+            </Form>
+        </Card>
     );
 };
