3cf0745785ce42b43e1289f63b16cf820dd87a83
Alexander Korobov <dktwho@mail.ru>
2024-01-29 14:38:06 +0300

    feat: finish component EditEmployee with handleEditUser

--- client/src/pages/edit-employee/EditEmployee.tsx
+++ client/src/pages/edit-employee/EditEmployee.tsx
@@ -1,7 +1,51 @@
+import {useNavigate, useParams} from "react-router-dom";
+import {useState} from "react";
+import {useEditEmployeeMutation, useGetEmployeeQuery} from "../../app/services/employees";
+import {Layout} from "../../components/layout/layout";
+import {Row} from "antd";
+import {EmployeeForm} from "../../components/employee-form/EmployeeForm";
+import {Employee} from "@prisma/client";
+import {Paths} from "../../paths";
+import {isErrorWithMessage} from "../../utils/isErrorWithMessage";
+
 export const EditEmployee = () => {
+    const navigate = useNavigate()
+    const params = useParams<{ id: string }>()
+    const [error, setError] = useState('')
+    const {data, isLoading} = useGetEmployeeQuery(params.id || '')
+    const [editEmployee] = useEditEmployeeMutation();
+
+    if (isLoading) {
+        return <span>Загрузка</span>
+    }
+
+    const handleEditUser = async (employee: Employee) => {
+        try {
+            const editedEmployee = {
+                ...data,
+                ...employee
+            }
+            await editEmployee(editedEmployee).unwrap();
+            navigate(`${Paths.status}/updated`)
+        } catch (e) {
+            const mayBeError = isErrorWithMessage(e)
+            if (mayBeError) {
+                setError(e.data.message)
+            } else {
+                setError('Неизвестная ошибка')
+            }
+
+        }
+    }
     return (
-        <div>
-            EditEmployee
-        </div>
+        <Layout>
+            <Row align={'middle'} justify={'center'}>
+                <EmployeeForm onFinish={handleEditUser}
+                              btnText={'Редактировать'}
+                              title={'Редактировать'}
+                              error={error}
+                              employee={data}/>
+            </Row>
+        </Layout>
     );
 };
